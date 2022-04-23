afa1f045d8e6eba20446cbda794f8541471843ea
Presentation12 <joaocarlosapresentacao@gmail.com>
2022-04-23 14:12:38 +0100

    menu conta user

---
+++ Interface/exemplo.js
@@ -0,0 +1,15 @@
+function mostraPass() 
+{
+
+  var x = document.getElementById("pass");
+
+  if (x.type === "password") 
+  {
+    x.type = "text";
+  }
+
+  else 
+  {
+    x.type = "password";
+  }
+}
