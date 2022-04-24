522c49454bfd78b58ff6e265de273599ef627c18
Presentation12 <joaocarlosapresentacao@gmail.com>
2022-04-24 14:27:08 +0100

    drop menu

--- Interface/detalhes.js
+++ Interface/detalhes.js
@@ -1,7 +1,7 @@
-function mostraPass() 
+function mostraPass(pass) 
 {
 
-  var x = document.getElementById("pass");
+  var x = document.getElementById(pass);
 
   if (x.type === "password") 
   {
@@ -12,4 +12,26 @@ function mostraPass()
   {
     x.type = "password";
   }
+}
+
+function editable() {
+  x=document.getElementById("nome")
+  x.disabled = false;
+  x=document.getElementById("apelido")
+  x.disabled = false;
+  x=document.getElementById("email")
+  x.disabled = false;
+  x=document.getElementById("contacto")
+  x.disabled = false;
+}
+
+function nonEditable() {
+  x=document.getElementById("nome")
+  x.disabled = true;
+  x=document.getElementById("apelido")
+  x.disabled = true;
+  x=document.getElementById("email")
+  x.disabled = true;
+  x=document.getElementById("contacto")
+  x.disabled = true;
 }
